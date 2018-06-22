8ccb91995614aaf75e080cd48b34a03552fb1fae
thomasioos <thomas.ioos@laposte.net>
2018-06-22 17:24:30 +0200

    control a LCD display via gpio

--- codeIHM.py
+++ codeIHM.py
@@ -2,6 +2,37 @@ import sys
 from PyQt4 import QtGui
 import interface
 from tsl2561 import TSL2561
+# import Adafruit_BBIO.PWM as PWM
+
+
+#Default period 10Khz
+def managePWM(period=100000, duty=5 ):
+    duty = duty *(period/100)
+    print("period: " , period)
+    print("duty: " , duty)
+    write_pwm(period, duty)
+
+
+def init_pwm():
+    file = open("/sys/devices/bone_capemgr.9/slots", "w")
+    if file is None:
+        print("Cape not found")
+    file.write("am33xx_pwm")
+    file.flush()
+    file.write("bone_pwm_P9_14")
+    file.close()
+
+def write_pwm(period,duty):
+    #init_pwm()
+    pol = open("/sys/devices/ocp.3//pwm_test_P9_14.14/polarity","w")
+    dut = open("/sys/devices/ocp.3//pwm_test_P9_14.14/duty","w")
+    per = open("/sys/devices/ocp.3//pwm_test_P9_14.14/period","w")
+    pol.write("0")
+    pol.close()
+    dut.write(str(duty))
+    dut.close()
+    per.write(str(period))
+    per.close()
 
 class Interface(QtGui.QMainWindow, interface.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -9,20 +40,113 @@ class Interface(QtGui.QMainWindow, interface.Ui_MainWindow):
         super(Interface, self).__init__(parent)
         # Sets up layout and widgets that are defined
         self.setupUi(self)
+
+	#RAZ de l'afficheur
+	self.i = 0
+	self.LCD_command()
+	self.lcdNumber.setHexMode()
+
+	#attach function to button
         self.perso_button.clicked.connect(self.personalize)
-        # self.perso_button.clicked.connect(self.personalize)
-        # self.UART_checkbox.stateChanged.connect(self.state)
-        # self.SPI_checkbox.stateChanged.connect(self.state)
-        # self.ANALOG_checkbox.stateChanged.connect(self.state)
-        self.I2C_checkbox.stateChanged.connect(self.state)
+	self.RUN_button.clicked.connect(self.run)
+	self.plus_button.clicked.connect(self.plus)
+	self.minus_button.clicked.connect(self.minus)
+
+	#attach fonction to pota
+        self.pota.valueChanged.connect(self.PWM_generator)
+
+    def run(self):
+	if(self.UART_checkbox.isChecked()):
+            self.UART()
+
+        if(self.I2C_checkbox.isChecked()):
+            self.I2C()
+
+        if(self.ANALOG_checkbox.isChecked()):
+            self.AnalogRead()
+
+        if(self.SPI_checkbox.isChecked()):
+            self.SPI()
+
+    def minus(self):
+	self.i = self.i - 1
+	if self.i == -1:
+	    self.i = 15
+	print hex(self.i)
+	self.LCD_command()
+
+    def plus(self):
+	self.i = self.i + 1
+	if self.i ==16:
+	    self.i = 0
+	print hex(self.i)
+	self.LCD_command()
+
+    def LCD_command(self):
+	self.lcdNumber.display(self.i)
+
+	#code la pin A
+	f = open("/sys/class/gpio/gpio30/value","w")
+	if self.i not in (1,3,5,7,9,11,13,15):
+	    f.write("0")
+	else:
+	    f.write("1")
+	f.close()
+
+	#code la pin B
+	f = open("/sys/class/gpio/gpio60/value","w")
+	if self.i not in (2,3,6,7,10,11,14,15):
+            f.write("0")
+        else:
+            f.write("1")
+        f.close()
+
+        #code la pin C
+        f = open("/sys/class/gpio/gpio31/value","w")
+        if self.i not in (4,5,6,7,12,13,14,15):
+            f.write("0")
+        else:
+            f.write("1")
+        f.close()
+
+        #code la pin D
+        f = open("/sys/class/gpio/gpio50/value","w")
+        if self.i not in (8,9,10,11,12,13,14,15):
+            f.write("0")
+        else:
+            f.write("1")
+        f.close()
 
     def personalize(self):
         print("ok")
 
-    def state(self):
+    def PWM_generator(self):
+        print(self.pota.value())
+	duty = int(self.pota.value()*10)
+        managePWM(duty=duty)
+
+    def plotWindow(self):
+        self.qwtPlot.setLabel("left","PWM_generator")
+        self.qwtPlot.addLegend()
+
+    def AnalogRead(self):
+	f = open("/sys/devices/ocp.3/helper.14/AIN0","r")
+	ana = f.read()
+	f.close()
+	self.input_text.append(ana)
+
+    def I2C(self):
         print("I2C : " , self.I2C_checkbox.isChecked())
         tsl = TSL2561(debug=True)
-        print(tsl.lux())
+        self.input_text.append("I2C : " + str(tsl.lux()))
+
+    def UART(self):
+        print("UART : " , self.UART_checkbox.isChecked())
+        self.input_text.append("UART")
+
+    def SPI(self):
+        print("SPI : " , self.SPI_checkbox.isChecked())
+        self.input_text.append("SPI")
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
--- interface.py
+++ interface.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'interface.ui'
+# Form implementation generated from reading ui file 'new_interface.ui'
 #
-# Created: Tue Jun 12 13:33:37 2018
+# Created: Tue Jun 19 12:47:15 2018
 #      by: PyQt4 UI code generator 4.11.2
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
+import PyQt4.Qwt5 as Qwt
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -26,11 +27,14 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(945, 667)
+        MainWindow.resize(1382, 735)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.gridLayout = QtGui.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.pota = Qwt.QwtKnob(self.centralwidget)
+        self.pota.setObjectName(_fromUtf8("pota"))
+        self.gridLayout.addWidget(self.pota, 2, 1, 1, 1)
         self.gridLayout_2 = QtGui.QGridLayout()
         self.gridLayout_2.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
         self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
@@ -46,22 +50,37 @@ class Ui_MainWindow(object):
         self.UART_checkbox = QtGui.QCheckBox(self.centralwidget)
         self.UART_checkbox.setObjectName(_fromUtf8("UART_checkbox"))
         self.gridLayout_2.addWidget(self.UART_checkbox, 1, 0, 1, 1)
-        self.gridLayout.addLayout(self.gridLayout_2, 0, 6, 1, 1)
-        self.perso_button = QtGui.QPushButton(self.centralwidget)
-        self.perso_button.setObjectName(_fromUtf8("perso_button"))
-        self.gridLayout.addWidget(self.perso_button, 1, 6, 1, 1)
-        self.dial = QtGui.QDial(self.centralwidget)
-        self.dial.setObjectName(_fromUtf8("dial"))
-        self.gridLayout.addWidget(self.dial, 10, 0, 1, 15)
+        self.gridLayout.addLayout(self.gridLayout_2, 0, 0, 1, 1)
         self.input_text = QtGui.QTextBrowser(self.centralwidget)
         self.input_text.setObjectName(_fromUtf8("input_text"))
-        self.gridLayout.addWidget(self.input_text, 0, 14, 1, 1)
+        self.gridLayout.addWidget(self.input_text, 0, 3, 1, 1)
+        self.perso_button = QtGui.QPushButton(self.centralwidget)
+        self.perso_button.setObjectName(_fromUtf8("perso_button"))
+        self.gridLayout.addWidget(self.perso_button, 1, 0, 1, 1)
         self.RUN_button = QtGui.QPushButton(self.centralwidget)
         self.RUN_button.setObjectName(_fromUtf8("RUN_button"))
-        self.gridLayout.addWidget(self.RUN_button, 1, 10, 1, 1)
+        self.gridLayout.addWidget(self.RUN_button, 1, 1, 1, 1)
+        self.qwtPlot = Qwt.QwtPlot(self.centralwidget)
+        self.qwtPlot.setObjectName(_fromUtf8("qwtPlot"))
+        self.gridLayout.addWidget(self.qwtPlot, 2, 3, 1, 1)
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout_2.addWidget(self.label)
+        self.plus_button = QtGui.QPushButton(self.centralwidget)
+        self.plus_button.setObjectName(_fromUtf8("plus_button"))
+        self.verticalLayout_2.addWidget(self.plus_button)
+        self.minus_button = QtGui.QPushButton(self.centralwidget)
+        self.minus_button.setObjectName(_fromUtf8("minus_button"))
+        self.verticalLayout_2.addWidget(self.minus_button)
+        self.lcdNumber = QtGui.QLCDNumber(self.centralwidget)
+        self.lcdNumber.setObjectName(_fromUtf8("lcdNumber"))
+        self.verticalLayout_2.addWidget(self.lcdNumber)
+        self.gridLayout.addLayout(self.verticalLayout_2, 2, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 945, 19))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1382, 38))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         self.menuMenu = QtGui.QMenu(self.menubar)
         self.menuMenu.setObjectName(_fromUtf8("menuMenu"))
@@ -87,19 +106,12 @@ class Ui_MainWindow(object):
         self.I2C_checkbox.setText(_translate("MainWindow", "I2C", None))
         self.SPI_checkbox.setText(_translate("MainWindow", "SPI", None))
         self.UART_checkbox.setText(_translate("MainWindow", "UART", None))
-        self.perso_button.setText(_translate("MainWindow", "PERSONNALISATION", None))
+        self.perso_button.setText(_translate("MainWindow", "PERSONNALIZATION", None))
         self.RUN_button.setText(_translate("MainWindow", "OK", None))
+        self.label.setText(_translate("MainWindow", "    LCD COMMAND", None))
+        self.plus_button.setText(_translate("MainWindow", "+", None))
+        self.minus_button.setText(_translate("MainWindow", "-", None))
         self.menuMenu.setTitle(_translate("MainWindow", "Menu", None))
         self.actionSettings.setText(_translate("MainWindow", "Settings", None))
         self.actionExit.setText(_translate("MainWindow", "Exit", None))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
 
